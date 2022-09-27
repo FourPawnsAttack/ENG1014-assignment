6d4cfc30dc9c717e0ee330409b891cfc282d26d8
4PawnsAttack <lucaswee626@gmail.com>
2022-09-27 23:39:27 +0800

    updates

--- Q2.m
+++ Q2.m
@@ -4,24 +4,29 @@
 % DO NOT clear, close or clc inside this script
 % Apply good programming practices
 %
-% Name :
-% ID   :
-% Date Modified : 
+% Name : Wee Jun Lin
+% ID   : 32620861
+% Date Modified : 27/9/2022
 
 fprintf('\n Q2 \n\n')
 %%
 %Add your code here
-date_matrix = datenum(time_year,time_month,time_day);
+% variables 
+date_matrix = datenum(time_year,time_month,time_day); % convert date to single serial number
 wind_all = [wind_ararat,wind_boco,wind_silver];
 counter_ararat = 0;
 counter_boco = 0;
 counter_silver = 0;
+counter_tie = 0;
 
 % find the distribution of time when each each site has the largest wind speed
 for i = 1:size(wind_ararat)
+    % find the index where the max windspeed occurs
     [M,I] = max(wind_all(i,:));
-    disp(I)
-    if I == 1
+    % find ties 
+    if (wind_all(i,1) == wind_all(i,2) && I == 1) || (wind_all(i,2) == wind_all(i,3) && I == 2) || (wind_all(i,1) == wind_all(i,3) && I == 1)
+        counter_tie = counter_tie + 1;
+    elseif I == 1
         counter_ararat = counter_ararat + 1;
     elseif I == 2
         counter_boco = counter_boco + 1;
@@ -29,6 +34,7 @@ for i = 1:size(wind_ararat)
         counter_silver = counter_silver + 1;
     end
 end
+counter_all = [counter_ararat,counter_boco,counter_silver,counter_tie];
 
 %Print results
 
@@ -53,12 +59,18 @@ datetick('x')
 xlabel('Date'); ylabel('Wind Speed');
 title('Wind Speed vs Time (Silverton)');
 
-figure(2)
-
-
-
-
 
 %Plotting for (b)
-figure(3)
+% plot pie chart for part b
+figure(2)
+
+pie(counter_all)
+title('Percentage of time each site had the largest wind speed')
+legend('Ararat','Boco Rock','Silverton','Tie')
+
 %Plotting for (c)
+
+figure(3)
+
+
+
--- Q3.m
+++ Q3.m
@@ -11,7 +11,20 @@
 fprintf('\n Q3 \n\n')
 %%
 %Add your code here
+%
+data_file = importdata('avg_wind_data.txt').data;
+height = data_file(:,1);
+avg_ararat = data_file(:,2);
+avg_silver = data_file(:,3);
+avg_boco = data_file(:,4);
 
+
+plot(height,avg_ararat,'bo','MarkerSize',6)
+hold on 
+plot(height,avg_silver,'r*','MarkerSize',9)
+hold on
+plot(height,avg_boco,'m--','MarkerSize',9)
+legend('Ararat','Silverton','Boco Rock')
 
 %Print results
 
